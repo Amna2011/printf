b7e6ee41f72be17172ccabcd482fec147c2a940b
Amna2011 <amnataha2011@gmail.com>
2024-01-27 07:57:24 -0800

    mmm

--- main.h
+++ main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
-int _putchar(char c);
+char _putchar(char c);
 int print_format(char specifier, va_list ap);
 int print_char(int c);
 int print_str(char *str);
