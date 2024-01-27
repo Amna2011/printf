6a3ec87f476cb3689973dfd9c7fe955452352e82
malloudaya <malloudaya@gmail.com>
2024-01-27 14:45:23 -0800

    task0_update

--- _printf.c
+++ _printf.c
@@ -1,24 +1,53 @@
 #include "main.h"
 #include <stdarg.h>
+#include <unistd.h>
+
 /**
- * _printf - produces output according to format
- * @format: format string containing zero or more directives
+ * _printf - Produces output according to a format.
+ * @format: The format string containing conversion specifiers.
  *
- * Return: the number of characters printed, excluding null byte
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int count;
+    va_list args;
+    int count = 0;
+    char spec;
 
-	count = 0;
-	va_start(ap, format);
-	while (*format != '\0')
-		if (*format == '%')
-			count += print_format(*(++format), ap);
-		else
-			count += _putchar(format);
-		++format;
-	va_end(ap);
-	return (count);
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%'))
+        {
+            format++;
+            spec = *format;
+
+            if (spec == 'c')
+            {
+                char c = va_arg(args, int);
+                count += write(1, &c, 1);
+            }
+            else if (spec == 's')
+            {
+                char *str = va_arg(args, char*);
+                while (*str)
+                    count += write(1, str++, 1);
+            }
+            else if (spec == '%')
+            {
+                count += write(1, "%", 1);
+            }
+        }
+        else
+        {
+            count += write(1, format, 1);
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
 }
--- main.h
+++ main.h
@@ -1,14 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-int print_format(char specifier, va_list ap);
-int print_char(int c);
-int print_str(char *str);
-int print_int(long n, int base);
+
 #endif /* MAIN_H */
