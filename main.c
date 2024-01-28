7b349ea5b05e9fc29a1099f3c837dce7d0626cbe
malloudaya <malloudaya@gmail.com>
2024-01-27 16:12:39 -0800

    main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+        char c;
+
+        c = 'a';
+        printf("hello %c\n", c);
+        _printf("hello %c\n", c);
+        printf("your name is %s\n", "aya");
+        _printf("your name is %s\n", "aya");
+        return (0);
+}
