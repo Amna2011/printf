c4992d9e3597371091c2edf8c823afece3204ea2
malloudaya <malloudaya@gmail.com>
2024-01-28 07:21:36 -0800

    update

--- _printf.c
+++ _printf.c
@@ -12,51 +12,27 @@ int _printf(const char *format, ...)
 	va_list args;
 	int count;
 	char spec;
-	int num;
 	char c;
 	char *str;
 
 	count = 0;
 	va_start(args, format);
 	while (*format)
-	{
 		if (*format == '%' && (*(format + 1) == 'c'
-			|| *(format + 1) == 's' || *(format + 1) == '%'
-			|| *(format + 1) == 'd' || *(format + 1) == 'i'))
-		{
+			|| *(format + 1) == 's' || *(format + 1) == '%'))
 			format++;
 			spec = *format;
-			if (spec == 'd')
-			{
-				num = va_arg(args, int);
-				count += _printint(num);
-			}
-			else if (spec == 'i')
-			{
-				num = va_arg(args, int);
-				count += _printint(num);
-			}
-			else if (spec == 'c')
-			{
+			if (spec == 'c')
 				c = va_arg(args, int);
 				count += _printchar(c);
-			}
 			else if (spec == 's')
-			{
 				str = va_arg(args, char*);
 				count += _printstr(str);
-			}
 			else if (spec == '%')
-			{
 				count += _printchar('%');
-			}
-		}
 		else
-		{
 			count += _putchar(*format);
-		}
 		format++;
-	}
 	va_end(args);
 	return (count);
 }
