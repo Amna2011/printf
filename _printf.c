c91b7b5df11e91b6f5c0a823eef46c6509169110
malloudaya <malloudaya@gmail.com>
2024-01-28 07:26:56 -0800

    task 1

--- _printf.c
+++ _printf.c
@@ -12,27 +12,46 @@ int _printf(const char *format, ...)
 	va_list args;
 	int count;
 	char spec;
+	int num;
 	char c;
 	char *str;
 
 	count = 0;
 	va_start(args, format);
 	while (*format)
+	{
 		if (*format == '%' && (*(format + 1) == 'c'
-			|| *(format + 1) == 's' || *(format + 1) == '%'))
+			|| *(format + 1) == 's' || *(format + 1) == '%'
+			|| *(format + 1) == 'd' || *(format + 1) == 'i'))
+		{
 			format++;
 			spec = *format;
-			if (spec == 'c')
+			if (spec == 'd' || spec == 'i')
+			{
+				num = va_arg(args, int);
+				count += _printint(num);
+			}
+			else if (spec == 'c')
+			{
 				c = va_arg(args, int);
 				count += _printchar(c);
+			}
 			else if (spec == 's')
+			{
 				str = va_arg(args, char*);
 				count += _printstr(str);
+			}
 			else if (spec == '%')
+			{
 				count += _printchar('%');
+			}
+		}
 		else
+		{
 			count += _putchar(*format);
+		}
 		format++;
+	}
 	va_end(args);
 	return (count);
 }
